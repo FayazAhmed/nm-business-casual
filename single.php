41730e1a841d15c129ce69a31b9bbf2e1f2d53ae
FayazAhmed <fayaz.nmedia@gmail.com>
2016-05-18 20:41:30 +0500

    adding button in index.php posts and changing in single.php tag,author,date add

--- single.php
+++ single.php
@@ -13,7 +13,11 @@
 			        	    ?>
 			        	    
 			        	    <h2 class="intro-text text-center"><strong><?php the_title();?></strong></h2>
-			        	    <span><?php the_date(); ?></span>
+			        	    <div class="label label-default">
+								<span>Posted by <?php the_author_posts_link(); ?> at <?php the_time( 'F j, Y g:i a'); ?></span>
+								<span>Taged by: <?php the_tags( '', ', ', '<br />' ); ?> </span>
+								<span>Catergory of: <?php the_category(' ,'); ?></span>
+							</div>
 			        	    <hr class="visible-xs">
 			        	    <?php the_content(); ?>
 			        	    
