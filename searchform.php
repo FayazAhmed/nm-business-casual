dd7ce3a576599e4f5314129d5dadb837c11509ad
FayazAhmed <fayaz.nmedia@gmail.com>
2016-05-21 20:41:35 +0500

    styling tha (ul>li>a) widgets li color

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form method="get" id="searchform" class="navbar-form navbar-right searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<div class="form-inline">
+	<div class="form-group">
 		<input type="text" name="s" class="form-control" placeholder="Search Here.." value="<?php echo get_search_query(); ?>">
 	</div>
-	<button type="submit" id="searchsubmit" class="btn btn-primary">Submit</button>
+	<button type="submit"  class="btn btn-primary serch-button">Submit</button>
 </form> 
